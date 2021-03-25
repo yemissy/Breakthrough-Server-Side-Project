94cab61d4cf2e313290dcfb426e494b3c14244ff
ShrutiTamrakarTuladhar <t.shruti98@gmail.com>
2021-03-25 14:42:09 -0400

    some notes added

--- modles/users.js
+++ modles/users.js
@@ -1,6 +1,16 @@
 class Users {
 
-// CRUD (create, read, update, delete)  
+// CRUD (create, read, update, delete) 
+
+/* .get - read , .post - create, .put - update 
+.delete - detele  
+
+
+To create a user /site/:siteId/user
+
+
+
+*/
 
 
 
